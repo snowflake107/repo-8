c7b1aad3538dc9f7426670b5eb58608507fbe86d
timkim <timk@adobe.com>
2023-07-12 13:59:34 -0700

    Fix up iframe logic when given api paths

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,16 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
     if (isBrowser) {
         let apiLinkLocation = window.location.href.split('#');
         if(apiLinkLocation.length > 1){
-            // set the iframe to the right location
-            console.log('setting iframe src to: ' + document.querySelector.all('iframe')[1].src + '/' + apiLinkLocation)
-            document.querySelector.all('iframe')[1].src = document.querySelector.all('iframe')[1].src + '/' + apiLinkLocation;
+            window.onload = function () {
+                // set the iframe to the right location
+                if (window.location.host.indexOf("adobe.com") >= 0 || window.location.host.indexOf("github.io") >= 0) {
+                    console.log('setting iframe src to: ' + document.querySelectorAll('iframe')[1].src + '#' + apiLinkLocation[1])
+                    document.querySelectorAll('iframe')[1].src = document.querySelectorAll('iframe')[1].src + '#' + apiLinkLocation[1];
+                } else {
+                    console.log('setting iframe src to: ' + document.querySelectorAll('iframe')[0].src + '#' + apiLinkLocation[1])
+                    document.querySelectorAll('iframe')[0].src = document.querySelectorAll('iframe')[0].src + '#' + apiLinkLocation[1];
+                }
+            }
         }
     }
 }
