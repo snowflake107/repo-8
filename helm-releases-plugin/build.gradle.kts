91039dec7eb2a42b623fb0f6fbbb8524ca23d0b1
imanushin <igor.manushin@citi.com>
2023-08-17 12:59:51 +0100

    add display name

--- helm-releases-plugin/build.gradle.kts
+++ helm-releases-plugin/build.gradle.kts
@@ -22,7 +22,7 @@ gradlePlugin {
     plugins {
         create("helmReleasesPlugin") {
             id = "com.citi.helm-releases"
-            displayName = "Helm Releases Plugin"
+            displayName = "Helm Releases"
             implementationClass = "com.citi.gradle.plugins.helm.release.HelmReleasesPlugin"
             description = "Extension for Gradle Helm Plugin. Supports charts installation/uninstallation."
             tags.addAll("helm", "release", "install", "uninstall", "cloud", "kubernetes")
